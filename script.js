cf2ac7b979c2e0990e56ceb1fa058b5a6d48bcce
adolfomen9 <adolfomen9@gmail.com>
2025-08-04 18:09:40 -0600

    modificacion de script.js para descarga de pdf

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.getElementById("btnImprimir").addEventListener("click", function() {
-  window.print();
+  const elemento = document.getElementById("formCasoUso");
+
+  const opciones = {
+    margin: 10,
+    filename: 'caso_de_uso.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+  };
+
+  html2pdf().set(opciones).from(elemento).save();
 });
